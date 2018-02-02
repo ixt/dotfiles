4493d729b79ee4e7a094738be90b86e36a116cfc
NfN Orange <orange@ff4500.red>
2018-02-02 20:39:50 +0000

    spring cleaning

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# 2017-12-27 20:56:29
-
-# setting up pgp 
-gpg --card-status
-gpg --import ~/Keys/orangeatff4500dotred.asc
-
-echo ". ~/.bash_aliases" >> ~/.bashrc
-. ~/.bash_aliases
